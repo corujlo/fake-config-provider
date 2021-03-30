90c88530bf3aaa70fa68756361a7b2248769ef61
Jesús López Becerra <jesuslb@ext.inditex.com>
2021-03-30 13:46:52 +0200

    First commit

---
+++ src/main/java/net/coru/fakeconfigprovider/FakeConfigProviderApplication.java
@@ -0,0 +1,13 @@
+package net.coru.fakeconfigprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeConfigProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakeConfigProviderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/coru/fakeconfigprovider/FakeConfigProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package net.coru.fakeconfigprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeConfigProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
